81c2d289378b9d9cac2527fd0f7339a9d96c72f2
ayahhow <prince_8tears@icloud.com>
2018-05-31 01:18:49 +0900

    ユーザーひとりいる・今からユーザーページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup', to: 'users#new'
   get '/contact', to: 'static_pages#contact'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
